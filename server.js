3861ba6b5f6da717c44f324fa2a68646c04e7c5f
Mauricio B de Paula <mauricio.browne@gmail.com>
2018-07-27 22:31:20 -0700

    Reorganized code, added express server to code. Commented out some useful code.

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 // Importing Configuration class from different file
 var Config = require("./Config");
 var pizzapi = require("pizzapi");
+var express = require("express");
+
+var app = express();
 
 var storeID;
-
 var data = new Config();
 var myStore;
 var order;
 var fullAddress = new pizzapi.Address(data.address);
 
-// Method gets StoreID variable based on nearest store to Configured Address
-var getStoreID = function(){
+// Method gets StoreID variable based on nearest store and calls the method
+// responsible for setting up the order. It also validates the order.
+var createOrder = function(){
   pizzapi.Util.findNearbyStores(
     data.address, data.method, function(storeData){
+      console.log("--- Retrieving store number for address ---");
       storeID = storeData.result.Stores[0].StoreID;
-      console.log("StoreID for address: " + storeID);
+      console.log("\tStoreID: " + storeID);
+      console.log("--- Setting up order ---");
+      setUpOrder();
+      validateOrder();
     }
   );
 }
@@ -26,7 +33,7 @@ var getMenu = function(){
   });
 }
 
-// Method used to place order
+// Method used to initialize and set up the order object
 var setUpOrder = function(){
   myStore = new pizzapi.Store(0);
   myStore.ID = storeID;
@@ -85,6 +92,50 @@ var priceOrder = function(){
   });
 }
 
+
+// --- Express Routings --- //
+app.get("/", function(req, res){
+  console.log("* The main page has been loaded by a client.");
+});
+
+
+// Initializing the server
+var PORT = 5000;
+app.listen(PORT, function(){
+  console.log("Listening on port %d", PORT);
+});
+// Called after the server is initialized
+createOrder();
+
+
+// addItemToOrder("14SCVEGGIE");
+// priceOrder();
+// addItemToOrder("14SCVEGGIE");
+// validateOrder();
+// priceOrder();
+// WE WILL GET RID OF THE TIME OUT. ORDER WILL BE INITIALIZED WHEN THE SERVER IS INITIALIZED.
+// DIFFERENT ROUTES WILL HANDLE DIFFERENT METHODS. ONCE THEY ARE CALLED THE ORDER OBJECT
+// WILL HAVE BEEN PREVIOUSLY SET UP.
+// // Created timer to allow program to fetch store ID from API before intializing the store variable
+// setTimeout(function(){
+//   setUpOrder();
+//   addItemToOrder("14SCVEGGIE");
+//   addItemToOrder("14SCVEGGIE");
+//   validateOrder();
+//   priceOrder();
+// // console.log("NEW SECTION:");
+// // var cardNumber = '4100123422343234';
+// //
+// // var cardInfo = new order.PaymentObject();
+// // cardInfo.Amount = order.Amounts.Customer;
+// // cardInfo.Number = cardNumber;
+// // cardInfo.CardType = order.validateCC(cardNumber);
+// // cardInfo.Expiration = '0115';//  01/15 just the numbers "01/15".replace(/\D/g,'');
+// // cardInfo.SecurityCode = '777';
+// // cardInfo.PostalCode = '90210'; // Billing Zipcode
+// }, 5000);
+
+
 // Code used to log the menu to console
 // var testStore = new pizzapi.Store(10053);
 // testStore.ID = 10053;
@@ -96,12 +147,3 @@ var priceOrder = function(){
 //   console.log("Output:");
 //   console.log(menu.result);
 // }, 5000);
-
-getStoreID();
-setTimeout(function(){
-  setUpOrder();
-  addItemToOrder("14SCVEGGIE");
-  addItemToOrder("14SCVEGGIE");
-  validateOrder();
-  priceOrder();
-}, 5000);
